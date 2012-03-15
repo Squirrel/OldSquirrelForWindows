c721a41a1312c14bd4b0e954246b0f44495849a0
Paul Betts <paul@paulbetts.org>
2012-03-15 15:49:39 -0600

    First test!

---
+++ src/NSync.Core/ReleasePackage.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NSync.Core
+{
+    public class ReleasePackage : IEnableLogger
+    {
+        public ReleasePackage(string inputPackageFile)
+        {
+        }
+
+        public void CreateReleasePackage(string outputFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NSync.Tests/Core/ReleasePackageTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using NSync.Core;
+using NSync.Tests.TestHelpers;
+using NuGet;
+using Xunit;
+
+namespace NSync.Tests.Core
+{
+    public class ReleasePackageTests : IEnableLogger
+    {
+        [Fact]
+        public void ReleasePackageIntegrationTest()
+        {
+            var inputPackage = IntegrationTestHelper.GetPath("fixtures", "NSync.Core.1.0.0.0.nupkg");
+            var outputPackage = Path.GetTempFileName() + ".nupkg";
+            var fixture = new ReleasePackage(inputPackage);
+
+            fixture.CreateReleasePackage(outputPackage);
+
+            var pkg = new ZipPackage(outputPackage);
+            pkg.References.Count().ShouldEqual(0);
+        }
+    }
+}
