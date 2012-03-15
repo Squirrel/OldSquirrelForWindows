98ccd131a70dcfa0a09197696f50893151408ed0
Paul Betts <paul@paulbetts.org>
2012-03-15 15:45:04 -0600

    Add NLog Helper

---
+++ src/NSync.Core/NLogHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using ReactiveUI;
+using LogLevel = NLog.LogLevel;
+
+namespace NSync.Core
+{
+    public interface IEnableLogger { }
+
+    public static class LoggerMixin
+    {
+        static LoggerMixin()
+        {
+            if (RxApp.InUnitTestRunner())
+            {
+                var target = new ConsoleTarget() { Layout = "${level:uppercase=true} ${logger}: ${message}${onexception:inner=${newline}${exception:format=tostring}}" };
+                var config = new LoggingConfiguration();
+
+                config.LoggingRules.Add(new LoggingRule("*", LogLevel.Info, target));
+                LogManager.Configuration = config;
+            }
+        }
+
+        public static Logger Log<T>(this T This) where T : IEnableLogger
+        {
+            return LogManager.GetLogger(typeof(T).FullName);
+        }
+    }
+}
