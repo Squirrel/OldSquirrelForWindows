c503c22167e4d388dcbf3c47fe97e9f1cfc5cb44
Paul Betts <paul@paulbetts.org>
2012-03-15 18:06:28 -0700

    Move some code to a utility class

---
+++ src/NSync.Core/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NSync.Core
+{
+    public static class Utility
+    {
+        public static IEnumerable<FileInfo> GetAllFilesRecursively(string rootPath)
+        {
+            var di = new DirectoryInfo(rootPath);
+
+            return di.GetDirectories()
+                .SelectMany(x => GetAllFilesRecursively(x.FullName))
+                .Concat(di.GetFiles());
+        }
+    }
+}
