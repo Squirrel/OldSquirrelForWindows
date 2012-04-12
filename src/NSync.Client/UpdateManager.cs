d17201f89f9f0b3cc1375e5fce48cc0a273c5425
Paul Betts <paul@paulbetts.org>
2012-04-11 23:30:31 -0700

    Write the top-level function for checking for updates

--- src/NSync.Client/UpdateManager.cs
+++ src/NSync.Client/UpdateManager.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using NSync.Core;
 
 namespace NSync.Client
 {
@@ -7,18 +12,39 @@ namespace NSync.Client
     {
         Func<string, Stream> openPath;
         Func<string, IObservable<string>> downloadUrl;
+        string updateUrl;
 
         public UpdateManager(string url, 
             Func<string, Stream> openPathMock = null,
             Func<string, IObservable<string>> downloadUrlMock = null)
         {
+            updateUrl = url;
             openPath = openPathMock;
             downloadUrl = downloadUrlMock;
         }
 
         public IObservable<UpdateInfo> CheckForUpdate()
         {
-            throw new NotImplementedException();
+            IEnumerable<ReleaseEntry> localReleases;
+
+            using (var sr = new StreamReader(openPath(Path.Combine("packages", "RELEASES")))) {
+                localReleases = ReleaseEntry.ParseReleaseFile(sr.ReadToEnd());
+            }
+
+            var ret = downloadUrl(updateUrl)
+                .Select(ReleaseEntry.ParseReleaseFile)
+                .Select(releases => determineUpdateInfo(localReleases, releases))
+                .Multicast(new AsyncSubject<UpdateInfo>());
+
+            ret.Connect();
+            return ret;
+        }
+
+        UpdateInfo determineUpdateInfo(IEnumerable<ReleaseEntry> localReleases, IEnumerable<ReleaseEntry> remoteReleases)
+        {
+            if (localReleases.Count() == remoteReleases.Count()) {
+                return null;
+            }
         }
     }
 
