6d4cb0525aaccb3c645541120f4af72b88b58b4c
Paul Betts <paul@paulbetts.org>
2012-04-11 20:30:22 -0700

    Parse Release entries
    This adds a class to parse release file entries, corresponding to the end
    of specs/Implementation.md

---
+++ src/NSync.Core/ReleaseEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NSync.Core
+{
+    public class ReleaseEntry : IEnableLogger
+    {
+        public string SHA1 { get; protected set; }
+        public string Filename { get; protected set; }
+        public long Filesize { get; protected set; }
+        public bool IsDelta { get; protected set; }
+
+        protected ReleaseEntry(string sha1, string filename, long filesize, bool isDelta)
+        {
+            SHA1 = sha1; Filename = filename; Filesize = filesize; IsDelta = isDelta;
+        }
+
+        static readonly Regex entryRegex = new Regex(@"^([0-9a-fA-F]{40})\s+(\S+)\s+(\d+)$");
+        public static ReleaseEntry ParseReleaseEntry(string entry)
+        {
+            var m = entryRegex.Match(entry);
+            if (!m.Success) {
+                return null;
+            }
+
+            if (m.Groups.Count != 4) {
+                return null;
+            }
+
+            long size = Int64.Parse(m.Groups[3].Value);
+            bool isDelta = m.Groups[2].Value.EndsWith(".delta", StringComparison.InvariantCultureIgnoreCase);
+            return new ReleaseEntry(m.Groups[1].Value, m.Groups[2].Value, size, isDelta);
+        }
+    }
+}
---
+++ src/NSync.Tests/Core/ReleaseEntryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSync.Core;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NSync.Tests.Core
+{
+    public class ReleaseEntryTests
+    {
+        [Theory]
+        [InlineData("94689fede03fed7ab59c24337673a27837f0c3ec  MyCoolApp-1.0.nupkg  1004502", "MyCoolApp-1.0.nupkg", 1004502)]
+        [InlineData("3a2eadd15dd984e4559f2b4d790ec8badaeb6a39  MyCoolApp-1.1.nupkg  1040561", "MyCoolApp-1.1.nupkg", 1040561)]
+        [InlineData("14db31d2647c6d2284882a2e101924a9c409ee67  MyCoolApp-1.1.nupkg.delta  80396", "MyCoolApp-1.1.nupkg.delta", 80396)]
+        public void ParseValidReleaseEntryLines(string releaseEntry, string fileName, long fileSize)
+        {
+            var fixture = ReleaseEntry.ParseReleaseEntry(releaseEntry);
+            Assert.Equal(fixture.Filename, fileName);
+            Assert.Equal(fixture.Filesize, fileSize);
+        }
+    }
+}
