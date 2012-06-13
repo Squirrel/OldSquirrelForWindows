b89fe7dcfabcbc752a9180959245f3d317daa209
Paul Betts <paul@paulbetts.org>
2012-06-13 16:09:20 -0700

    Get a real path out of ShortcutCreationRequest

--- src/NSync.Client/IAppSetup.cs
+++ src/NSync.Client/IAppSetup.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using NSync.Core;
 
 namespace NSync.Client
 {
-    [Flags]
     public enum ShortcutCreationLocation {
-        StartMenu = 1 << 0,
-        Desktop = 1 << 1,
-        Custom = 1 << 2,
+        StartMenu,
+        Desktop,
+        Custom,
     }
 
-    public sealed class ShortcutCreationRequest
+    public sealed class ShortcutCreationRequest : IEquatable<ShortcutCreationRequest>
     {
         // Shortcut Details
         public string Title { get; set; }
-        public string Path { get; set; }
+        public string TargetPath { get; set; }
         public string Arguments { get; set; }
         public string WorkingDirectory { get; set; }
         public string IconLibrary { get; set; }
@@ -25,6 +26,66 @@ namespace NSync.Client
         // Where to put the shortcut
         public ShortcutCreationLocation CreationLocation { get; set; }
         public string CustomLocation { get; set; }
+
+        public string GetLinkTarget(string applicationName, bool createDirectoryIfNecessary = false)
+        {
+            var dir = default(string);
+
+            switch(CreationLocation) {
+            case ShortcutCreationLocation.Desktop:
+                dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                break;
+            case ShortcutCreationLocation.StartMenu:
+                dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), applicationName);
+                break;
+            case ShortcutCreationLocation.Custom:
+                dir = (new FileInfo(CustomLocation)).DirectoryName;
+                break;
+            }
+
+            if (createDirectoryIfNecessary && Directory.Exists(dir)) {
+                (new DirectoryInfo(dir)).CreateRecursive();
+            }
+
+            return Path.Combine(dir, Title + ".lnk");
+        }
+
+        #region Boring Equality Stuff
+        public bool Equals(ShortcutCreationRequest other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Title, other.Title) && Equals(CreationLocation, other.CreationLocation) && string.Equals(CustomLocation, other.CustomLocation);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (ShortcutCreationRequest)) return false;
+            return Equals((ShortcutCreationRequest) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked {
+                int hashCode = (Title != null ? Title.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ CreationLocation.GetHashCode();
+                hashCode = (hashCode*397) ^ (CustomLocation != null ? CustomLocation.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(ShortcutCreationRequest left, ShortcutCreationRequest right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ShortcutCreationRequest left, ShortcutCreationRequest right)
+        {
+            return !Equals(left, right);
+        }
+        #endregion
     }
 
     public interface IAppSetup
