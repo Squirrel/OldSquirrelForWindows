b5829e2fc6359d941cc13cb267dbb8ec641fc2bd
Paul Betts <paul@paulbetts.org>
2012-06-14 13:57:46 -0700

    Add another test stub

--- src/NSync.Tests/Client/UpdateManagerTests.cs
+++ src/NSync.Tests/Client/UpdateManagerTests.cs
@@ -415,6 +415,12 @@ namespace NSync.Tests.Client
             {
                 throw new NotImplementedException();
             }
+
+            [Fact]
+            public void DeletedShortcutsShouldntBeRecreatedOnUpgrade()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
