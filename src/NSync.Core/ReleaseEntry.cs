ec54bad7991890f7a6b87666862ae4064bbd63c0
Paul Betts <paul@paulbetts.org>
2012-06-15 16:51:24 -0700

    Nuke debug code

--- src/NSync.Core/ReleaseEntry.cs
+++ src/NSync.Core/ReleaseEntry.cs
@@ -11,16 +11,9 @@ namespace NSync.Core
     public class ReleaseEntry : IEnableLogger
     {
         public string SHA1 { get; protected set; }
+        public string Filename { get; set; }
         public long Filesize { get; protected set; }
         public bool IsDelta { get; protected set; }
-
-        string _Filename;
-        public string Filename {
-            get {
-                return _Filename;
-            }
-            set { _Filename = value;  }
-        }
 
         protected ReleaseEntry(string sha1, string filename, long filesize, bool isDelta)
         {
