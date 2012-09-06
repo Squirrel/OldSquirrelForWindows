22156495e13c78066d389ac49c2127790f542d88
Paul Betts <paul@paulbetts.org>
2012-09-06 16:21:20 -0700

    Build NuGet packages for Shimmer.Client and Shimmer.Core
    Write proper metadata for Shimmer's Client and Core packages

--- src/Shimmer.Core/Properties/AssemblyInfo.cs
+++ src/Shimmer.Core/Properties/AssemblyInfo.cs
@@ -6,13 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Shimmer.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core routines for manipulating and creating Shimmer release packages")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shimmer.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("GitHub")]
+[assembly: AssemblyProduct("Shimmer")]
+[assembly: AssemblyCopyright("Copyright © 2012 GitHub")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae7f53ed-c5e5-4e23-b69e-03337b03fa60")]
+[assembly: Guid("c1175afc-25a1-4fd7-9615-5b233762cf02")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
