bd3ab16d0b9c994bfa5fa6da4883fd1354ba1d83
Paul Betts <paul@paulbetts.org>
2012-09-20 15:39:39 -0700

    Forget about singleton silliness

--- src/Shimmer.WiXUi/ViewModels/WixUiBootstrapper.cs
+++ src/Shimmer.WiXUi/ViewModels/WixUiBootstrapper.cs
@@ -31,7 +31,7 @@ namespace Shimmer.WiXUi.ViewModels
         public WixUiBootstrapper(IWiXEvents wixEvents, TinyIoCContainer testKernel = null, IRoutingState router = null)
         {
             Kernel = testKernel ?? createDefaultKernel();
-            Kernel.Register<IWixUiBootstrapper>(this).AsSingleton();
+            Kernel.Register<IWixUiBootstrapper>(this);
             Kernel.Register<IScreen>(this);
             Kernel.Register(wixEvents);
 
