583fc70f8a3d3f28e797af081b73087011949264
Paul Betts <paul@paulbetts.org>
2012-09-20 22:25:51 -0700

    Add some empty views

---
+++ src/Shimmer.WiXUi/Views/WelcomeView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using Shimmer.Client.WiXUi;
+using Shimmer.WiXUi.ViewModels;
+
+namespace Shimmer.WiXUi.Views
+{
+    /// <summary>
+    /// Interaction logic for WelcomeView.xaml
+    /// </summary>
+    public partial class WelcomeView : UserControl, IWelcomeView
+    {
+        public WelcomeView()
+        {
+            InitializeComponent();
+        }
+
+        public IWelcomeViewModel ViewModel {
+            get { return (IWelcomeViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(IWelcomeViewModel), typeof(WelcomeView), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (IWelcomeViewModel) value; }
+        }
+
+    }
+}
