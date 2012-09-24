c2bce5422f5c9701d237138b60e24ee5b069367b
Paul Betts <paul@paulbetts.org>
2012-09-24 13:36:34 -0700

    Stub out a bunch of tests for WiXUiBootstrapper

---
+++ src/Shimmer.Tests/WiXUi/WiXUiBootstrapperTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Shimmer.Tests.WiXUi
+{
+    public class WiXUiBootstrapperTests
+    {
+        [Fact]
+        public void RouteToErrorViewWhenThingsGoPearShaped()
+        {
+            throw new NotImplementedException();
+        }
+
+        //
+        // DetectPackageComplete
+        //
+        
+        [Fact]
+        public void RouteToInstallOnDetectPackageComplete()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void RouteToUninstallOnDetectPackageComplete()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IfAppIsAlreadyInstalledRunTheApp()
+        {
+            throw new NotImplementedException();
+        }
+
+        //
+        // PlanComplete
+        //
+
+        [Fact]
+        public void EigenUpdateWithoutUpdateURL()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void EigenUpdateWithUpdateURL()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UpdateReportsProgress()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void InstallHandlesAccessDenied()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UninstallRemovesEverything()
+        {
+            throw new NotImplementedException();
+        }
+
+        //
+        // Helper methods
+        //
+
+        [Fact]
+        public void DetermineFxVersionFromPackageTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void RegisterExtensionDllsFindsExtensions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DefaultTypesShouldntStepOnExtensionRegisteredTypes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
