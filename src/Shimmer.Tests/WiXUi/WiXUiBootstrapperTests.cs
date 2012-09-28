6ef0dad5292ed0e345f83710a97edfde30d1c10c
Paul Betts <paul@paulbetts.org>
2012-09-28 16:00:44 -0700

    Initial hack at testing executeInstall

--- src/Shimmer.Tests/WiXUi/WiXUiBootstrapperTests.cs
+++ src/Shimmer.Tests/WiXUi/WiXUiBootstrapperTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -12,6 +13,7 @@ using System.Text;
 using System.Threading;
 using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
 using Moq;
+using NuGet;
 using ReactiveUI;
 using ReactiveUI.Routing;
 using Shimmer.Client.WiXUi;
@@ -132,7 +134,40 @@ namespace Shimmer.Tests.WiXUi
         [Fact]
         public void EigenUpdateWithoutUpdateURL()
         {
-            throw new NotImplementedException();
+            string dir;
+            string outDir;
+
+            var events = new Mock<IWiXEvents>();
+            events.SetupGet(x => x.DetectPackageCompleteObs).Returns(Observable.Never<DetectPackageCompleteEventArgs>());
+            events.SetupGet(x => x.ErrorObs).Returns(Observable.Never<ErrorEventArgs>());
+            events.SetupGet(x => x.PlanCompleteObs).Returns(Observable.Never<PlanCompleteEventArgs>());
+            events.SetupGet(x => x.ApplyCompleteObs).Returns(Observable.Never<ApplyCompleteEventArgs>());
+
+            events.SetupGet(x => x.DisplayMode).Returns(Display.Full);
+            events.SetupGet(x => x.Action).Returns(LaunchAction.Uninstall);
+
+            var engine = new Mock<IEngine>();
+            engine.Setup(x => x.Plan(LaunchAction.Uninstall)).Verifiable();
+            events.SetupGet(x => x.Engine).Returns(engine.Object);
+
+            using (Utility.WithTempDirectory(out outDir))
+            using (withFakeInstallDirectory(out dir)) {
+                var fixture = ExposedObject.From(new WixUiBootstrapper(events.Object, currentAssemblyDir: dir));
+                var pkg = new ZipPackage(Path.Combine(dir, "SampleUpdatingApp.1.1.0.0.nupkg"));
+                var progress = new Subject<int>();
+
+                var progressValues = new List<int>();
+                progress.Subscribe(progressValues.Add);
+
+                // NB: The casts are here so that the dynamic binder doens't get
+                // fooled into trying to match against the wrong signature
+                IObservable<Unit> ret = fixture.executeInstall(dir, (IPackage) pkg, (IObserver<int>) progress, outDir);
+                ret.First();
+
+                // Progress should be monotonically increasing
+                progressValues.Count.ShouldBeGreaterThan(2);
+                progressValues.Zip(progressValues.Skip(1), (prev, cur) => cur - prev).All(x => x > 0).ShouldBeTrue();
+            }
         }
 
         [Fact]
