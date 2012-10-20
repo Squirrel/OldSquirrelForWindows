3e5ded95333cf90dfbb82618640d21bacf23ebf6
Paul Betts <paul@paulbetts.org>
2012-10-19 17:37:51 -0700

    Add methods to help people who don't grok Rx

--- src/Shimmer.Client/IUpdateManager.cs
+++ src/Shimmer.Client/IUpdateManager.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
 using Shimmer.Core;
 
 namespace Shimmer.Client
@@ -82,17 +85,81 @@ namespace Shimmer.Client
         }
     }
 
-    public static class UpdateManagerMixins
+    public static class RxHatersMixin
     {
-        public static IObservable<ReleaseEntry> UpdateApp(this IUpdateManager This)
+        public static Task<ReleaseEntry> UpdateApp(this IUpdateManager This, Action<int> progress)
         {
-            var ret = This.CheckForUpdate(false, null)
-                .SelectMany(x => This.DownloadReleases(x.ReleasesToApply, null).TakeLast(1).Select(_ => x))
-                .SelectMany(x => This.ApplyReleases(x, null).TakeLast(1).Select(_ => x.ReleasesToApply.MaxBy(y => y.Version).LastOrDefault()))
+            var checkSubj = new Subject<int>();
+            var downloadSubj = new Subject<int>();
+            var applySubj = new Subject<int>();
+
+            var ret = This.CheckForUpdate(false, checkSubj)
+                .SelectMany(x => This.DownloadReleases(x.ReleasesToApply, downloadSubj).TakeLast(1).Select(_ => x))
+                .SelectMany(x => This.ApplyReleases(x, applySubj).TakeLast(1).Select(_ => x.ReleasesToApply.MaxBy(y => y.Version).LastOrDefault()))
                 .PublishLast();
 
+            var allProgress = Observable.Merge(
+                    checkSubj.Select(x => (double)x / 3.0),
+                    downloadSubj.Select(x => (double)x / 3.0),
+                    applySubj.Select(x => (double)x / 3.0))
+                .Scan(0.0, (acc, x) => acc + x);
+
+            allProgress.Subscribe(x => progress((int) x));
+
             ret.Connect();
-            return ret;
+            return ret.ToTask();
+        }
+
+        /// <summary>
+        /// Fetch the remote store for updates and compare against the current 
+        /// version to determine what updates to download.
+        /// </summary>
+        /// <param name="ignoreDeltaUpdates">Set this flag if applying a release
+        /// fails to fall back to a full release, which takes longer to download
+        /// but is less error-prone.</param>
+        /// <param name="progress">An Action which can be used to report Progress - 
+        /// will return values from 0-100</param>
+        /// <returns>An UpdateInfo object representing the updates to install.
+        /// </returns>
+        public static Task<UpdateInfo> CheckForUpdate(this IUpdateManager This, bool ignoreDeltaUpdates, Action<int> progress)
+        {
+            var subj = new Subject<int>();
+            subj.Subscribe(progress);
+            return This.CheckForUpdate(ignoreDeltaUpdates, subj).ToTask();
+        }
+
+        /// <summary>
+        /// Download a list of releases into the local package directory.
+        /// </summary>
+        /// <param name="releasesToDownload">The list of releases to download, 
+        /// almost always from UpdateInfo.ReleasesToApply.</param>
+        /// <param name="progress">An Action which can be used to report Progress - 
+        /// will return values from 0-100</param>
+        /// <returns>A completion Task<returns>
+        public static Task DownloadReleases(this IUpdateManager This, IEnumerable<ReleaseEntry> releasesToDownload, Action<int> progress)
+        {
+            var subj = new Subject<int>();
+            subj.Subscribe(progress, ex => { });
+
+            return This.DownloadReleases(releasesToDownload, subj).ToTask();
+        }
+
+        /// <summary>
+        /// Take an already downloaded set of releases and apply them, 
+        /// copying in the new files from the NuGet package and rewriting 
+        /// the application shortcuts.
+        /// </summary>
+        /// <param name="updateInfo">The UpdateInfo instance acquired from 
+        /// CheckForUpdate</param>
+        /// <param name="progress">An Action which can be used to report Progress - 
+        /// will return values from 0-100</param>
+        /// <returns>A list of EXEs that should be started if this is a new 
+        /// installation.</returns>
+        public static Task<List<string>> ApplyReleases(this IUpdateManager This, UpdateInfo updateInfo, Action<int> progress)
+        {
+            var subj = new Subject<int>();
+            subj.Subscribe(progress, ex => { });
+            return This.ApplyReleases(updateInfo, subj).ToTask();
         }
     }
 }
