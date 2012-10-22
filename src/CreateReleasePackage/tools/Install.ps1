19fe095cddfb1af61d36b555115dc98141e53d13
Paul Betts <paul@paulbetts.org>
2012-10-22 12:12:06 -0700

    Tools-only metapackages are disallowed in NuGet. Hack hack hack.

---
+++ src/CreateReleasePackage/tools/Install.ps1
@@ -0,0 +1,3 @@
+﻿## http://stackoverflow.com/questions/5935162/nuget-error-external-packages-cannot-depend-on-packages-that-target-projects-w
+param($installPath, $toolsPath, $package, $project)
+$project.Object.Project.ProjectItems.Item("keep.me").Delete()
