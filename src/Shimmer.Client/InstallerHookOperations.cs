065508582d460ddf14a83bcbccce35e60f3c9a7f
Paul Betts <paul@paulbetts.org>
2013-02-14 15:39:02 -0800

    Fix AppDomain serialization bugs

---
+++ src/Shimmer.Client/InstallerHookOperations.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Reflection;
+using ReactiveUIMicro;
+using Shimmer.Core;
+
+namespace Shimmer.Client
+{
+    [Serializable]
+    class InstallerHookOperations
+    {
+        readonly IRxUIFullLogger log;
+        readonly IFileSystemFactory fileSystem;
+        readonly string applicationName;
+
+        public InstallerHookOperations(IRxUIFullLogger log, IFileSystemFactory fileSystem, string applicationName)
+        {
+            this.log = log;
+            this.fileSystem = fileSystem;
+            this.applicationName = applicationName;
+        }
+
+        public IEnumerable<string> RunAppSetupInstallers(PostInstallInfo info)
+        {
+            var appSetups = default(IEnumerable<IAppSetup>);
+
+            try {
+                appSetups = findAppSetupsToRun(info.NewAppDirectoryRoot);
+            } catch (UnauthorizedAccessException ex) {
+                log.ErrorException("Failed to load IAppSetups in post-install due to access denied", ex);
+                return new string[0];
+            }
+
+            return appSetups
+                .Select(app => installAppVersion(app, info.NewCurrentVersion, info.ShortcutRequestsToIgnore, info.IsFirstInstall))
+                .Where(x => x != null)
+                .ToArray();
+        }
+
+        public IEnumerable<ShortcutCreationRequest> RunAppSetupCleanups(string fullDirectoryPath)
+        {
+            var dirName = Path.GetFileName(fullDirectoryPath);
+            var ver = new Version(dirName.Replace("app-", ""));
+
+            var apps = default(IEnumerable<IAppSetup>);
+            try {
+                apps = findAppSetupsToRun(fullDirectoryPath);
+            } catch (UnauthorizedAccessException ex) {
+                log.ErrorException("Couldn't run cleanups", ex);
+                return Enumerable.Empty<ShortcutCreationRequest>();
+            }
+
+            var ret = apps.SelectMany(app => uninstallAppVersion(app, ver)).ToArray();
+
+            return ret;
+        }
+
+        IEnumerable<ShortcutCreationRequest> uninstallAppVersion(IAppSetup app, Version ver)
+        {
+            try {
+                app.OnVersionUninstalling(ver);
+            } catch (Exception ex) {
+                log.ErrorException("App threw exception on uninstall:  " + app.GetType().FullName, ex);
+            }
+
+            var shortcuts = Enumerable.Empty<ShortcutCreationRequest>();
+            try {
+                shortcuts = app.GetAppShortcutList();
+            } catch (Exception ex) {
+                log.ErrorException("App threw exception on shortcut uninstall:  " + app.GetType().FullName, ex);
+            }
+
+            // Get the list of shortcuts that *should've* been there, but aren't;
+            // this means that the user deleted them by hand and that they should 
+            // stay dead
+            return shortcuts.Aggregate(new List<ShortcutCreationRequest>(), (acc, x) => {
+                                                                                            var path = x.GetLinkTarget(applicationName);
+
+                                                                                            var fi = fileSystem.GetFileInfo(path);
+                                                                                            if (fi.Exists) {
+                                                                                                fi.Delete();
+                                                                                            } else {
+                                                                                                acc.Add(x);
+                                                                                            }
+
+                                                                                            return acc;
+            });
+        }
+
+        string installAppVersion(IAppSetup app, Version newCurrentVersion, IEnumerable<ShortcutCreationRequest> shortcutRequestsToIgnore, bool isFirstInstall)
+        {
+            try {
+                if (isFirstInstall) app.OnAppInstall();
+                app.OnVersionInstalled(newCurrentVersion);
+            } catch (Exception ex) {
+                log.ErrorException("App threw exception on install:  " + app.GetType().FullName, ex);
+                throw;
+            }
+
+            var shortcutList = Enumerable.Empty<ShortcutCreationRequest>();
+            try {
+                shortcutList = app.GetAppShortcutList();
+            } catch (Exception ex) {
+                log.ErrorException("App threw exception on shortcut uninstall:  " + app.GetType().FullName, ex);
+                throw;
+            }
+
+            shortcutList
+                .Where(x => !shortcutRequestsToIgnore.Contains(x))
+                .ForEach(x => {
+                                  var shortcut = x.GetLinkTarget(applicationName, true);
+
+                                  var fi = fileSystem.GetFileInfo(shortcut);
+                                  if (fi.Exists) fi.Delete();
+
+                                  fileSystem.CreateDirectoryRecursive(fi.Directory.FullName);
+
+                                  var sl = new ShellLink() {
+                                      Target = x.TargetPath,
+                                      IconPath = x.IconLibrary,
+                                      IconIndex = x.IconIndex,
+                                      Arguments = x.Arguments,
+                                      WorkingDirectory = x.WorkingDirectory,
+                                      Description = x.Description
+                                  };
+
+                                  sl.Save(shortcut);
+                });
+
+            return app.LaunchOnSetup ? app.Target : null;
+        }
+
+        IEnumerable<IAppSetup> findAppSetupsToRun(string appDirectory)
+        {
+            var allExeFiles = default(FileInfoBase[]);
+
+            try {
+                allExeFiles = fileSystem.GetDirectoryInfo(appDirectory).GetFiles("*.exe");
+            } catch (UnauthorizedAccessException ex) {
+                // NB: This can happen if we run into a MoveFileEx'd directory,
+                // where we can't even get the list of files in it.
+                log.WarnException("Couldn't search directory for IAppSetups: " + appDirectory, ex);
+                throw;
+            }
+
+            var locatedAppSetups = allExeFiles
+                .Select(x => loadAssemblyOrWhine(x.FullName)).Where(x => x != null)
+                .SelectMany(x => x.GetModules())
+                .SelectMany(x => {
+                                     try {
+                                         return x.GetTypes().Where(y => typeof (IAppSetup).IsAssignableFrom(y));
+                                     } catch (ReflectionTypeLoadException ex) {
+                                         log.WarnException("Couldn't load types from module", ex);
+                                         return Enumerable.Empty<Type>();
+                                     }
+                })
+                .Select(createInstanceOrWhine).Where(x => x != null)
+                .ToArray();
+
+            return locatedAppSetups.Length > 0
+                ? locatedAppSetups
+                : allExeFiles.Select(x => new DidntFollowInstructionsAppSetup(x.FullName)).ToArray();
+        }
+
+
+        IAppSetup createInstanceOrWhine(Type typeToCreate)
+        {
+            try {
+                return (IAppSetup) Activator.CreateInstance(typeToCreate);
+            }
+            catch (Exception ex) {
+                log.WarnException("Post-install: Failed to create type " + typeToCreate.FullName, ex);
+                return null;
+            }
+        }
+
+        Assembly loadAssemblyOrWhine(string fileToLoad)
+        {
+            try {
+                var ret = Assembly.LoadFile(fileToLoad);
+                return ret;
+            }
+            catch (Exception ex) {
+                log.WarnException("Post-install: load failed for " + fileToLoad, ex);
+                return null;
+            }
+        }
+    }
+}
--- src/Shimmer.Client/UpdateManager.cs
+++ src/Shimmer.Client/UpdateManager.cs
@@ -29,7 +29,6 @@ using StreamReader = System.IO.StreamReader;
 
 namespace Shimmer.Client
 {
-    [Serializable]
     public sealed class UpdateManager : IUpdateManager
     {
         readonly IRxUIFullLogger log;
@@ -431,6 +430,19 @@ namespace Shimmer.Client
             return runPostInstallOnDirectory(targetPath.FullName, isBootstrapping, newCurrentVersion, shortcutsToIgnore);
         }
 
+        List<string> runPostInstallOnDirectory(string newAppDirectoryRoot, bool isFirstInstall, Version newCurrentVersion, IEnumerable<ShortcutCreationRequest> shortcutRequestsToIgnore)
+        {
+            var postInstallInfo = new PostInstallInfo {
+                NewAppDirectoryRoot = newAppDirectoryRoot,
+                IsFirstInstall = isFirstInstall,
+                NewCurrentVersion = newCurrentVersion,
+                ShortcutRequestsToIgnore = shortcutRequestsToIgnore.ToArray()
+            };
+
+            var installerHooks = new InstallerHookOperations(log, fileSystem, applicationName);
+            return AppDomainHelper.ExecuteInNewAppDomain(postInstallInfo, installerHooks.RunAppSetupInstallers).ToList();
+        }
+
         static bool pathIsInFrameworkProfile(IPackageFile packageFile, FrameworkVersion appFrameworkVersion)
         {
             if (!packageFile.Path.StartsWith("lib", StringComparison.InvariantCultureIgnoreCase)) {
@@ -493,7 +505,9 @@ namespace Shimmer.Client
                 .OrderBy(x => x.Name)
                 .SelectMany(oldAppRoot => {
                     var path = oldAppRoot.FullName;
-                    var ret = AppDomainHelper.ExecuteInNewAppDomain(path, runAppSetupCleanups);
+                    var installerHooks = new InstallerHookOperations(log, fileSystem, applicationName);
+
+                    var ret = AppDomainHelper.ExecuteInNewAppDomain(path, installerHooks.RunAppSetupCleanups);
 
                     try {
                         Utility.DeleteDirectoryAtNextReboot(oldAppRoot.FullName);
@@ -548,180 +562,6 @@ namespace Shimmer.Client
             }
         }
 
-        IEnumerable<ShortcutCreationRequest> runAppSetupCleanups(string fullDirectoryPath)
-        {
-            var dirName = Path.GetFileName(fullDirectoryPath);
-            var ver = new Version(dirName.Replace("app-", ""));
-
-            var apps = default(IEnumerable<IAppSetup>);
-            try {
-                apps = findAppSetupsToRun(fullDirectoryPath);
-            } catch (UnauthorizedAccessException ex) {
-                log.ErrorException("Couldn't run cleanups", ex);
-                return Enumerable.Empty<ShortcutCreationRequest>();
-            }
-
-            var ret = apps.SelectMany(app => uninstallAppVersion(app, ver)).ToArray();
-
-            return ret;
-        }
-
-        IEnumerable<ShortcutCreationRequest> uninstallAppVersion(IAppSetup app, Version ver)
-        {
-            try {
-                app.OnVersionUninstalling(ver);
-            } catch (Exception ex) {
-                log.ErrorException("App threw exception on uninstall:  " + app.GetType().FullName, ex);
-            }
-
-            var shortcuts = Enumerable.Empty<ShortcutCreationRequest>();
-            try {
-                shortcuts = app.GetAppShortcutList();
-            } catch (Exception ex) {
-                log.ErrorException("App threw exception on shortcut uninstall:  " + app.GetType().FullName, ex);
-            }
-
-            // Get the list of shortcuts that *should've* been there, but aren't;
-            // this means that the user deleted them by hand and that they should 
-            // stay dead
-            return shortcuts.Aggregate(new List<ShortcutCreationRequest>(), (acc, x) => {
-                var path = x.GetLinkTarget(applicationName);
-
-                var fi = fileSystem.GetFileInfo(path);
-                if (fi.Exists) {
-                    fi.Delete();
-                } else {
-                    acc.Add(x);
-                }
-
-                return acc;
-            });
-        }
-
-        List<string> runPostInstallOnDirectory(string newAppDirectoryRoot, bool isFirstInstall, Version newCurrentVersion, IEnumerable<ShortcutCreationRequest> shortcutRequestsToIgnore)
-        {
-            var postInstallInfo = new PostInstallInfo {
-                NewAppDirectoryRoot = newAppDirectoryRoot,
-                IsFirstInstall = isFirstInstall,
-                NewCurrentVersion = newCurrentVersion,
-                ShortcutRequestsToIgnore = shortcutRequestsToIgnore.ToArray()
-            };
-
-            return AppDomainHelper.ExecuteInNewAppDomain(postInstallInfo, info => {
-                var appSetups = default(IEnumerable<IAppSetup>);
-
-                try {
-                    appSetups = findAppSetupsToRun(info.NewAppDirectoryRoot);
-                } catch (UnauthorizedAccessException ex) {
-                    log.ErrorException("Failed to load IAppSetups in post-install due to access denied", ex);
-                    return new string[0];
-                }
-
-                return appSetups
-                    .Select(app => installAppVersion(app, info.NewCurrentVersion, info.ShortcutRequestsToIgnore, info.IsFirstInstall))
-                    .Where(x => x != null)
-                    .ToArray();
-            }).ToList();
-        }
-
-        string installAppVersion(IAppSetup app, Version newCurrentVersion, IEnumerable<ShortcutCreationRequest> shortcutRequestsToIgnore, bool isFirstInstall)
-        {
-            try {
-                if (isFirstInstall) app.OnAppInstall();
-                app.OnVersionInstalled(newCurrentVersion);
-            } catch (Exception ex) {
-                log.ErrorException("App threw exception on install:  " + app.GetType().FullName, ex);
-                throw;
-            }
-
-            var shortcutList = Enumerable.Empty<ShortcutCreationRequest>();
-            try {
-                shortcutList = app.GetAppShortcutList();
-            } catch (Exception ex) {
-                log.ErrorException("App threw exception on shortcut uninstall:  " + app.GetType().FullName, ex);
-                throw;
-            }
-
-            shortcutList
-                .Where(x => !shortcutRequestsToIgnore.Contains(x))
-                .ForEach(x => {
-                    var shortcut = x.GetLinkTarget(applicationName, true);
-
-                    var fi = fileSystem.GetFileInfo(shortcut);
-                    if (fi.Exists) fi.Delete();
-
-                    fileSystem.CreateDirectoryRecursive(fi.Directory.FullName);
-
-                    var sl = new ShellLink() {
-                        Target = x.TargetPath,
-                        IconPath = x.IconLibrary,
-                        IconIndex = x.IconIndex,
-                        Arguments = x.Arguments,
-                        WorkingDirectory = x.WorkingDirectory,
-                        Description = x.Description
-                    };
-
-                    sl.Save(shortcut);
-                });
-
-            return app.LaunchOnSetup ? app.Target : null;
-        }
-
-        IEnumerable<IAppSetup> findAppSetupsToRun(string appDirectory)
-        {
-            var allExeFiles = default(FileInfoBase[]);
-
-            try {
-                allExeFiles = fileSystem.GetDirectoryInfo(appDirectory).GetFiles("*.exe");
-            } catch (UnauthorizedAccessException ex) {
-                // NB: This can happen if we run into a MoveFileEx'd directory,
-                // where we can't even get the list of files in it.
-                log.WarnException("Couldn't search directory for IAppSetups: " + appDirectory, ex);
-                throw;
-            }
-
-            var locatedAppSetups = allExeFiles
-                .Select(x => loadAssemblyOrWhine(x.FullName)).Where(x => x != null)
-                .SelectMany(x => x.GetModules())
-                .SelectMany(x => {
-                    try {
-                        return x.GetTypes().Where(y => typeof (IAppSetup).IsAssignableFrom(y));
-                    } catch (ReflectionTypeLoadException ex) {
-                        log.WarnException("Couldn't load types from module", ex);
-                        return Enumerable.Empty<Type>();
-                    }
-                })
-                .Select(createInstanceOrWhine).Where(x => x != null)
-                .ToArray();
-
-            return locatedAppSetups.Length > 0
-                ? locatedAppSetups
-                : allExeFiles.Select(x => new DidntFollowInstructionsAppSetup(x.FullName)).ToArray();
-        }
-
-        IAppSetup createInstanceOrWhine(Type typeToCreate)
-        {
-            try {
-                return (IAppSetup) Activator.CreateInstance(typeToCreate);
-            }
-            catch (Exception ex) {
-                log.WarnException("Post-install: Failed to create type " + typeToCreate.FullName, ex);
-                return null;
-            }
-        }
-
-        Assembly loadAssemblyOrWhine(string fileToLoad)
-        {
-            try {
-                var ret = Assembly.LoadFile(fileToLoad);
-                return ret;
-            }
-            catch (Exception ex) {
-                log.WarnException("Post-install: load failed for " + fileToLoad, ex);
-                return null;
-            }
-        }
-
         // NB: Once we uninstall the old version of the app, we try to schedule
         // it to be deleted at next reboot. Unfortunately, depending on whether
         // the user has admin permissions, this can fail. So as a failsafe,
@@ -744,8 +584,6 @@ namespace Shimmer.Client
                     .LoggedCatch<Unit, UpdateManager, UnauthorizedAccessException>(this, _ => Observable.Return(Unit.Default)))
                 .Aggregate(Unit.Default, (acc, x) => acc);
         }
-
-
     }
 
     public class DidntFollowInstructionsAppSetup : AppSetup
