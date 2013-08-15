a9a1a5aa50fd3a9e85c1f68344ac1c3ecc6a82e0
Brendan Forster <brendan@github.com>
2013-08-15 13:19:00 +1000

    shorthand to generate a new release

---
+++ Create-Release.ps1
@@ -0,0 +1,18 @@
+param(
+    [ValidateSet('Rebuild','Build', 'Clean')]
+    [string]
+    $build = "Rebuild"
+    ,
+    [ValidateSet('Debug', 'Release')]
+    [string]
+    $config = "Debug"
+)
+
+$rootFolder = split-path -parent $MyInvocation.MyCommand.Definition
+
+$binaries = "$rootFolder\bin\"
+
+if (Test-Path $binaries) { Remove-Item $binaries -Recurse -Force }
+
+. $rootFolder\script\Bump-Version.ps1 -Increment Patch
+. $rootFolder\Build-Solution.ps1 -Build $build -Config $config
