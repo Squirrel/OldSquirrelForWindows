433bc3a96f040e6a369e978738c4ae9f480d6056
Brendan Forster <brendan@github.com>
2013-08-23 10:08:39 +1000

    tidy up welcome view to investigate why the title isn't showing

--- src/Shimmer.WiXUi/Views/WelcomeView.xaml.cs
+++ src/Shimmer.WiXUi/Views/WelcomeView.xaml.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ReactiveUI;
 using ReactiveUI.Xaml;
-using Shimmer.Client.WiXUi;
 using Shimmer.WiXUi.ViewModels;
 
 namespace Shimmer.WiXUi.Views
 {
-    /// <summary>
-    /// Interaction logic for WelcomeView.xaml
-    /// </summary>
-    public partial class WelcomeView : UserControl, IViewFor<WelcomeViewModel>
+    public partial class WelcomeView : IViewFor<WelcomeViewModel>
     {
         public WelcomeView()
         {
@@ -35,6 +20,7 @@ namespace Shimmer.WiXUi.Views
             get { return (WelcomeViewModel)GetValue(ViewModelProperty); }
             set { SetValue(ViewModelProperty, value); }
         }
+
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register("ViewModel", typeof(WelcomeViewModel), typeof(WelcomeView), new PropertyMetadata(null));
 
@@ -42,6 +28,5 @@ namespace Shimmer.WiXUi.Views
             get { return ViewModel; }
             set { ViewModel = (WelcomeViewModel) value; }
         }
-
     }
 }
