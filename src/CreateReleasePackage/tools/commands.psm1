6eaa2f08e7082b55fe526209abdef69dab980e31
Brendan Forster <brendan@github.com>
2013-08-28 15:12:36 +1000

    moved Create-Release into a NuGet command

--- src/CreateReleasePackage/tools/commands.psm1
+++ src/CreateReleasePackage/tools/commands.psm1
@@ -1,5 +1,6 @@
 ﻿$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-
+$createReleasePackageExe = Join-Path $toolsDir "CreateReleasePackage.exe"
+    
 function Initialize-ProjectForShimmer {
     [CmdletBinding()]
     param (
@@ -27,12 +28,77 @@ function Initialize-ProjectForShimmer {
     $dte.ItemOperations.OpenFile($nuspecFile) | Out-Null
 }
 
+function Generate-TemplateFromPackage {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$packageFile,
+        [Parameter(Mandatory = $true)]
+        [string]$templateFile
+    )
+
+	$resultFile = & $createReleasePackageExe --preprocess-template $templateFile $pkg.FullName
+    $resultFile
+}
+
+function Create-ReleaseForProject {
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$solutionDir,
+		[Parameter(Mandatory = $true)]
+		[string]$buildDirectory
+	)
+
+	$releaseDir = Join-Path $solutionDir "Releases"
+	if (!(Test-Path $releaseDir)) { mkdir -p $releaseDir }
+
+	echo "Creating Release for $solutionDir => $releaseDir`n"
+
+	$nugetPackages = ls "$buildDirectory\*.nupkg" | ?{ $_.Name.EndsWith(".symbols.nupkg") -eq $false }
+
+	if ($nugetPackages.length -eq 0) {
+		throw "Shimmer couldn't find any .nupkg files in the folder $buildDirectory"
+	}
+
+	foreach($pkg in $nugetPackages) {
+        $pkgFullName = $pkg.FullName
+        echo "Found package $pkgFullName"
+
+		$packageDir = Join-Path $solutionDir "packages"
+		$fullRelease = & $createReleasePackageExe -o $releaseDir -p $packageDir $pkgFullName
+
+        ## NB: For absolutely zero reason whatsoever, $fullRelease ends up being the full path Three times
+        $fullRelease = $fullRelease.Split(" ")[0]
+		echo "Full release file at $fullRelease"
+
+        $candleTemplate = Generate-TemplateFromPackage $pkg.FullName "$toolsDir\template.wxs"
+        $wixTemplate = Join-Path $buildDirectory "template.wxs"
+        if (Test-Path $wixTemplate) { rm $wixTemplate | Out-Null }
+        mv $candleTemplate $wixTemplate | Out-Null
+
+		$defines = " -d`"ToolsDir=$toolsDir`"" + " -d`"NuGetFullPackage=$fullRelease`"" 
+
+		$candleExe = Join-Path $wixDir "candle.exe"
+		$lightExe = Join-Path $wixDir "light.exe"
+		
+		if (Test-Path "$buildDirectory\template.wixobj") {  rm "$buildDirectory\template.wixobj" | Out-Null }
+        echo "Running candle.exe"
+        & $candleExe "-d`"ToolsDir=$toolsDir`"" "-d`"ReleasesFile=$releaseDir\RELEASES`"" "-d`"NuGetFullPackage=$fullRelease`"" -out "$buildDirectory\template.wixobj" -arch x86 -ext "$wixDir\WixBalExtension.dll" -ext "$wixDir\WixUtilExtension.dll" $wixTemplate		
+        echo "Running light.exe"		
+        & $lightExe -out "$releaseDir\Setup.exe" -ext "$wixDir\WixBalExtension.dll" -ext "$wixDir\WixUtilExtension.dll" "$buildDirectory\template.wixobj"
+	}
+}
+
+
 function Publish-ShimmerRelease {
     [CmdletBinding()]
     param (
         [Parameter(Position=0, ValueFromPipeLine=$true)]
         [string] $ProjectName
     )
+
+    $wixDir = Join-Path $toolsDir "wix"
+    $support = Join-Path $toolsDir "support.ps1"
+    . $support
 
     if (-not $ProjectName) {
         $ProjectName = (Get-Project).Name
@@ -43,13 +109,12 @@ function Publish-ShimmerRelease {
     $solutionDir = (gci $dte.Solution.FullName).Directory
 
     $project = Get-Project $ProjectName
+
+    $projectDir = (gci $project.FullName).Directory
     $outputDir =  $project.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value
-
-    $createReleaseScript = Join-Path $toolsDir "Create-Release.ps1"
-
-    . $createReleaseScript -ProjectNameToBuild $ProjectName  `
-                           -SolutionDir $solutionDir `
-                           -BuildDirectory $outputDir
+    
+    Create-ReleaseForProject -SolutionDir $solutionDir `
+                             -BuildDirectory (Join-Path $projectDir $outputDir)
 }
 
 # Statement completion for project names
