cb3124b74274beee38718c47f5c9803e114993d3
Brendan Forster <brendan@github.com>
2013-09-05 16:49:49 +1000

    bump version to 0.6.14

--- src/Shimmer.Core/Properties/AssemblyInfo.cs
+++ src/Shimmer.Core/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.13")]
-[assembly: AssemblyFileVersion("0.6.13")]
-[assembly: AssemblyInformationalVersion("0.6.13")]
+[assembly: AssemblyVersion("0.6.14")]
+[assembly: AssemblyFileVersion("0.6.14")]
+[assembly: AssemblyInformationalVersion("0.6.14")]
 
 [assembly: InternalsVisibleTo("Shimmer.Client")]
--- src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
+++ src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Shimmer.WiXUi;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.13")]
-[assembly: AssemblyFileVersion("0.6.13")]
-[assembly: AssemblyInformationalVersion("0.6.13")]
+[assembly: AssemblyVersion("0.6.14")]
+[assembly: AssemblyFileVersion("0.6.14")]
+[assembly: AssemblyInformationalVersion("0.6.14")]
 
 [assembly:BootstrapperApplication(typeof(App))]
