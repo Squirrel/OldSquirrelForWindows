0c0c936b6a959d37dc2a951c54ce4d9ba8c92f94
Brendan Forster <brendan@github.com>
2013-09-08 18:03:05 +1000

    captured a failing test to illustrate the problem

---
+++ src/Shimmer.Tests/Core/ContentTypeTests.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Xml;
+using Shimmer.Core;
+using Shimmer.Tests.TestHelpers;
+using Xunit;
+
+namespace Shimmer.Tests.Core
+{
+    public class ContentTypeTests
+    {
+        [Fact]
+        public void SimpleFileIsProcessed()
+        {
+            var contentType = IntegrationTestHelper.GetPath("fixtures", "content-types", "basic.xml");
+            var tempFile = Path.GetTempFileName() + ".xml";
+
+            try {
+                File.Copy(contentType, tempFile);
+
+                var doc = new XmlDocument();
+                doc.Load(tempFile);
+
+                Assert.DoesNotThrow(() => ContentType.Merge(doc));
+            } finally {
+                File.Delete(tempFile);
+            }
+        }
+
+        [Fact]
+        public void ComplexFileIsProcessed()
+        {
+            var contentType = IntegrationTestHelper.GetPath("fixtures", "content-types", "complex.xml");
+            var tempFile = Path.GetTempFileName() + ".xml";
+
+            try
+            {
+                File.Copy(contentType, tempFile);
+
+                var doc = new XmlDocument();
+                doc.Load(tempFile);
+
+                Assert.DoesNotThrow(() => ContentType.Merge(doc));
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+    }
+}
