83dfaa10f976fa686b2a1261d116f2360eea83da
Brendan Forster <brendan@github.com>
2013-09-08 15:28:45 +1000

    extracted xunit tests to standalone script

---
+++ script/Run-UnitTests.ps1
@@ -0,0 +1,48 @@
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+$rootDirectory = join-path $scriptPath ..
+$srcFolder = join-path $scriptPath "..\src"
+
+function Run-XUnit([string]$project, [int]$timeoutDuration) {
+    $dll = "$rootDirectory\bin\$project.dll"
+
+    $xunitDirectory = Join-Path $rootDirectory ext\xunit
+    $consoleRunner = Join-Path $xunitDirectory xunit.console.clr4.x86.exe
+    $xml = Join-Path $rootDirectory "nunit-$project.xml"
+    $outputPath = [System.IO.Path]::GetTempFileName()
+
+    $args = $dll, "/noshadow", "/nunit", $xml, "/silent"
+    [object[]] $output = "$consoleRunner " + ($args -join " ")
+    $process = Start-Process -PassThru -NoNewWindow -RedirectStandardOutput $outputPath $consoleRunner ($args | %{ "`"$_`"" })
+    Wait-Process -InputObject $process -Timeout $timeoutDuration -ErrorAction SilentlyContinue
+    if ($process.HasExited) {
+        $output += Get-Content $outputPath
+        $exitCode = $process.ExitCode
+    } else {
+        $output += "Tests timed out. Backtrace:"
+        $output += Get-DotNetStack $process.Id
+        $exitCode = 9999
+    }
+    Stop-Process -InputObject $process
+    Remove-Item $outputPath
+
+    $result = New-Object System.Object
+    $result | Add-Member -Type NoteProperty -Name Output -Value $output
+    $result | Add-Member -Type NoteProperty -Name ExitCode -Value $exitCode
+    $result
+}
+
+$exitCode = 0
+
+Write-Output ""
+Write-Output "Running Shimmer.Tests..."
+$result = Run-XUnit Shimmer.Tests 180
+if ($result.ExitCode -eq 0) {
+    # Print out the test result summary.
+    Write-Output $result.Output[-1]
+} else {
+    $exitCode = $result.ExitCode
+    Write-Output $result.Output
+}
+Write-Output ""
+
+exit $exitCode
--- script/cibuild.ps1
+++ script/cibuild.ps1
@@ -20,53 +20,10 @@ $srcFolder = join-path $scriptPath "..\src"
 $projFile = join-path $srcFolder Shimmer.sln
 $nugetExe = join-path $srcFolder .nuget\NuGet.exe
 
-function Run-XUnit([string]$project, [int]$timeoutDuration) {
-    $dll = "$rootDirectory\bin\$project.dll"
-
-    $xunitDirectory = Join-Path $rootDirectory ext\xunit
-    $consoleRunner = Join-Path $xunitDirectory xunit.console.clr4.x86.exe
-    $xml = Join-Path $rootDirectory "nunit-$project.xml"
-    $outputPath = [System.IO.Path]::GetTempFileName()
-
-    $args = $dll, "/noshadow", "/nunit", $xml, "/silent"
-    [object[]] $output = "$consoleRunner " + ($args -join " ")
-    $process = Start-Process -PassThru -NoNewWindow -RedirectStandardOutput $outputPath $consoleRunner ($args | %{ "`"$_`"" })
-    Wait-Process -InputObject $process -Timeout $timeoutDuration -ErrorAction SilentlyContinue
-    if ($process.HasExited) {
-        $output += Get-Content $outputPath
-        $exitCode = $process.ExitCode
-    } else {
-        $output += "Tests timed out. Backtrace:"
-        $output += Get-DotNetStack $process.Id
-        $exitCode = 9999
-    }
-    Stop-Process -InputObject $process
-    Remove-Item $outputPath
-
-    $result = New-Object System.Object
-    $result | Add-Member -Type NoteProperty -Name Output -Value $output
-    $result | Add-Member -Type NoteProperty -Name ExitCode -Value $exitCode
-    $result
-}
-
 . $nugetExe config -Set Verbosity=quiet
 
 . $nugetExe restore $projFile
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /t:$build /p:Configuration=$config /verbosity:$MSBuildVerbosity
 
-$exitCode = 0
-
-Write-Output ""
-Write-Output "Running Shimmer.Tests..."
-$result = Run-XUnit Shimmer.Tests 180
-if ($result.ExitCode -eq 0) {
-    # Print out the test result summary.
-    Write-Output $result.Output[-1]
-} else {
-    $exitCode = $result.ExitCode
-    Write-Output $result.Output
-}
-Write-Output ""
-
-exit $exitCode
+& $scriptPath\Run-UnitTests.ps1
