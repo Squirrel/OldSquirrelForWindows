98983346c30cad194f4a09283bec06dc00217c1b
Brendan Forster <brendan@github.com>
2013-09-08 16:46:26 +1000

    a quick script to simplify publishing to NuGet

---
+++ script/publish.ps1
@@ -0,0 +1,15 @@
+﻿$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+$rootDirectory = join-path $scriptPath ..
+$binFolder = join-path $scriptPath "..\bin\"
+$nugetExe = join-path $scriptPath "..\src\.nuget\NuGet.exe"
+
+Write-Host $binFolder
+Write-Host $nugetExe
+
+$packages = Get-ChildItem $binFolder\*.nupkg -Exclude *.symbols.nupkg
+
+$packages | foreach { 
+ $file = $_.FullName
+ Write-Host "Publishing file to NuGet:" $file
+ . $nugetExe push $file -Timeout 600 -NonInteractive
+}
