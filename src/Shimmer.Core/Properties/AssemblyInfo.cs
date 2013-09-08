b7fbee63d3201823693821d8f1403753a77696ce
Brendan Forster <brendan@github.com>
2013-09-08 16:22:15 +1000

    version bump to 0.6.19

--- src/Shimmer.Core/Properties/AssemblyInfo.cs
+++ src/Shimmer.Core/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.18")]
-[assembly: AssemblyFileVersion("0.6.18")]
-[assembly: AssemblyInformationalVersion("0.6.18")]
+[assembly: AssemblyVersion("0.6.19")]
+[assembly: AssemblyFileVersion("0.6.19")]
+[assembly: AssemblyInformationalVersion("0.6.19")]
 
 [assembly: InternalsVisibleTo("Shimmer.Client")]
