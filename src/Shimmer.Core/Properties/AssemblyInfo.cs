bc0bad4e0e07bc680a22d2a6cf3c1980f7d1b7b7
Brendan Forster <brendan@github.com>
2013-09-10 10:48:20 +0930

    version bump to 0.6.20

--- src/Shimmer.Core/Properties/AssemblyInfo.cs
+++ src/Shimmer.Core/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.19")]
-[assembly: AssemblyFileVersion("0.6.19")]
-[assembly: AssemblyInformationalVersion("0.6.19")]
+[assembly: AssemblyVersion("0.6.20")]
+[assembly: AssemblyFileVersion("0.6.20")]
+[assembly: AssemblyInformationalVersion("0.6.20")]
 
 [assembly: InternalsVisibleTo("Shimmer.Client")]
 [assembly: InternalsVisibleTo("Shimmer.Tests")]
--- src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
+++ src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Shimmer.WiXUi;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.19")]
-[assembly: AssemblyFileVersion("0.6.19")]
-[assembly: AssemblyInformationalVersion("0.6.19")]
+[assembly: AssemblyVersion("0.6.20")]
+[assembly: AssemblyFileVersion("0.6.20")]
+[assembly: AssemblyInformationalVersion("0.6.20")]
 
 [assembly:BootstrapperApplication(typeof(App))]
