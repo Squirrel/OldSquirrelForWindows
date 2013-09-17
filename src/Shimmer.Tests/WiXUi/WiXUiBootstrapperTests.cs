598df59a70a7f1417207ea84dd5c51177459f46d
Brendan Forster <brendan@github.com>
2013-09-17 12:52:47 +0930

    stubbed a test for ensuring the right processes are started after install

--- src/Shimmer.Tests/WiXUi/WiXUiBootstrapperTests.cs
+++ src/Shimmer.Tests/WiXUi/WiXUiBootstrapperTests.cs
@@ -22,6 +22,7 @@ using Shimmer.Client.WiXUi;
 using Shimmer.Core;
 using Shimmer.Tests.TestHelpers;
 using Shimmer.WiXUi.ViewModels;
+using TinyIoC;
 using Xunit;
 
 using ErrorEventArgs = Microsoft.Tools.WindowsInstallerXml.Bootstrapper.ErrorEventArgs;
@@ -55,7 +56,7 @@ namespace Shimmer.Tests.WiXUi
                 router.GetCurrentViewModel().GetType().ShouldEqual(typeof(ErrorViewModel));
 
                 router.NavigateAndReset.Execute(RxApp.GetService<IWelcomeViewModel>());
-                error.OnNext(new ErrorEventArgs(ErrorType.ExePackage, "Foo", 
+                error.OnNext(new ErrorEventArgs(ErrorType.ExePackage, "Foo",
                     packHResultIntoIntEvenThoughItShouldntBeThere(0x80004005), "Noope", 0, new string[0], 0));
 
                 router.GetCurrentViewModel().GetType().ShouldEqual(typeof(ErrorViewModel));
@@ -65,7 +66,7 @@ namespace Shimmer.Tests.WiXUi
         //
         // DetectPackageComplete
         //
-        
+
         [Fact]
         public void RouteToInstallOnDetectPackageComplete()
         {
@@ -126,10 +127,78 @@ namespace Shimmer.Tests.WiXUi
             }
         }
 
-        [Fact(Skip = "TODO")]
+        [Fact]
         public void IfAppIsAlreadyInstalledRunTheApp()
         {
-            throw new NotImplementedException();
+            string dir;
+            using (IntegrationTestHelper.WithFakeInstallDirectory(out dir)) {
+
+                // install version 1
+                var firstRouter = new RoutingState();
+                var firstDetectPackage = new Subject<DetectPackageCompleteEventArgs>();
+                var firstPlanComplete = new Subject<PlanCompleteEventArgs>();
+                var firstApplyComplete = new Subject<ApplyCompleteEventArgs>();
+                var firstError = new Subject<ErrorEventArgs>();
+
+                var firstEvents = new Mock<IWiXEvents>();
+                firstEvents.SetupGet(x => x.DetectPackageCompleteObs).Returns(firstDetectPackage);
+                firstEvents.SetupGet(x => x.ErrorObs).Returns(firstError);
+                firstEvents.SetupGet(x => x.PlanCompleteObs).Returns(firstPlanComplete);
+                firstEvents.SetupGet(x => x.ApplyCompleteObs).Returns(firstApplyComplete);
+
+                firstEvents.SetupGet(x => x.DisplayMode).Returns(Display.Full);
+                firstEvents.SetupGet(x => x.Action).Returns(LaunchAction.Install);
+                firstEvents.SetupGet(x => x.Engine).Returns(Mock.Of<IEngine>());
+
+                var firstFixture = new WixUiBootstrapper(firstEvents.Object, null, firstRouter, null, dir);
+                RxApp.GetAllServices<ICreatesObservableForProperty>().Any().ShouldBeTrue();
+
+                firstDetectPackage.OnNext(new DetectPackageCompleteEventArgs("Foo", 0, PackageState.Absent));
+
+                var viewModel = firstRouter.GetCurrentViewModel() as WelcomeViewModel;
+                viewModel.ShouldProceed.Execute(null);
+
+                firstPlanComplete.OnNext(new PlanCompleteEventArgs(0));
+
+                // TODO: wait for *stuff*
+
+                firstApplyComplete.OnNext(new ApplyCompleteEventArgs(0, ApplyRestart.None));
+
+                // install version 1 again
+
+                var secondRouter = new RoutingState();
+                var secondDetectPackage = new Subject<DetectPackageCompleteEventArgs>();
+                var secondPlanComplete = new Subject<PlanCompleteEventArgs>();
+                var secondApplyComplete = new Subject<ApplyCompleteEventArgs>();
+                var secondError = new Subject<ErrorEventArgs>();
+
+                var secondEvents = new Mock<IWiXEvents>();
+                secondEvents.SetupGet(x => x.DetectPackageCompleteObs).Returns(secondDetectPackage);
+                secondEvents.SetupGet(x => x.ErrorObs).Returns(secondError);
+                secondEvents.SetupGet(x => x.PlanCompleteObs).Returns(secondPlanComplete);
+                secondEvents.SetupGet(x => x.ApplyCompleteObs).Returns(secondApplyComplete);
+
+                secondEvents.SetupGet(x => x.DisplayMode).Returns(Display.Full);
+                secondEvents.SetupGet(x => x.Action).Returns(LaunchAction.Install);
+                secondEvents.SetupGet(x => x.Engine).Returns(Mock.Of<IEngine>());
+
+                // install version 1 again
+                var secondFixture = new WixUiBootstrapper(secondEvents.Object, null, secondRouter, null, dir);
+                RxApp.GetAllServices<ICreatesObservableForProperty>().Any().ShouldBeTrue();
+
+                secondDetectPackage.OnNext(new DetectPackageCompleteEventArgs("Foo", 0, PackageState.Absent));
+
+                viewModel = secondRouter.GetCurrentViewModel() as WelcomeViewModel;
+                viewModel.ShouldProceed.Execute(null);
+
+                secondPlanComplete.OnNext(new PlanCompleteEventArgs(0));
+
+                // TODO: wait for *stuff*
+
+                secondApplyComplete.OnNext(new ApplyCompleteEventArgs(0, ApplyRestart.None));
+
+                Assert.False(true);
+            }
         }
 
         //
