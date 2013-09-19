780d2cc6cb6a2e3b5f56878c3b604cd1a03b2b69
Brendan Forster <brendan@github.com>
2013-09-19 09:35:19 +0930

    moving messages around for more readability

--- src/CreateReleasePackage/tools/Install.ps1
+++ src/CreateReleasePackage/tools/Install.ps1
@@ -19,9 +19,9 @@ function Initialize-Shimmer {
     $projectDir = (gci $project.FullName).Directory
     $nuspecFile = (Join-Path $projectDir "$ProjectName.nuspec")
 
-    Write-Message "Initializing project $ProjectName for installer and packaging"
-
     if (!(Test-Path $nuspecFile)) {
+        Write-Message "Initializing project $ProjectName for installer and packaging"
+
         Add-InstallerTemplate -Destination $nuspecFile -ProjectName $ProjectName
 
         Set-BuildPackage -Value $true -ProjectName $ProjectName
@@ -30,6 +30,10 @@ function Initialize-Shimmer {
 
         # open the nuspec file in the editor
         $dte.ItemOperations.OpenFile($nuspecFile) | Out-Null
+    } else {
+        Write-Message "You already have a nuspec file defined, no changes applied"
+
+        Write-Message "Run 'Enable-BuildPackage' to enable package creation for a project"
     }
 }
 
