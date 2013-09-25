9fb282f27ea011035e8b59eea5de8f8d40bdafce
Brendan Forster <brendan@github.com>
2013-09-25 11:09:36 +0930

    version bump to 0.6.34

--- src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
+++ src/Shimmer.WiXUi/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Shimmer.WiXUi;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.33")]
-[assembly: AssemblyFileVersion("0.6.33")]
-[assembly: AssemblyInformationalVersion("0.6.33")]
+[assembly: AssemblyVersion("0.6.34")]
+[assembly: AssemblyFileVersion("0.6.34")]
+[assembly: AssemblyInformationalVersion("0.6.34")]
 
 [assembly:BootstrapperApplication(typeof(App))]
