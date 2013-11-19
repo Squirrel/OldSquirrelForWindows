0d1bf965cf1216ff071aed5467d1d5eebe388c2a
Paul Betts <paul@paulbetts.org>
2013-11-19 11:50:54 -0800

    Implement check for updates

--- src/Squirrel.Updater/Program.cs
+++ src/Squirrel.Updater/Program.cs
@@ -1,10 +1,13 @@
-﻿using Mono.Options;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Options;
+using Squirrel.Client;
+using Newtonsoft.Json;
 
 namespace Squirrel.Updater
 {
@@ -14,11 +17,13 @@ namespace Squirrel.Updater
         {
             var command = "";
             var target = "";
+            var appName = default(string);
             var showHelp = false;
 
             var opts = new OptionSet() {
                 { "c|command=", "One of 'check' or 'update' to return the latest update information, or to perform the update", v => command = v },
                 { "t|update-target=", "The URL or directory to download updates from", v => target = v },
+                { "n|app-name=", "(Optional) The name of the application, only necessary if updater.exe isn't in the install directory", v => appName = v},
                 { "h|help", "Show this message and exit", v => showHelp = v != null },
             };
 
@@ -50,7 +55,37 @@ namespace Squirrel.Updater
                 return 0;
             }
 
-            return 0;
+            appName = appName ?? determineAppName();
+
+            if (command.ToLowerInvariant() == "check") {
+                var mgr = new UpdateManager(target, appName, FrameworkVersion.Net40);
+
+                var updateInfo = default(UpdateInfo);
+                try {
+                    updateInfo = mgr.CheckForUpdate().First();
+                } catch (Exception ex) {
+                    Console.WriteLine(JsonConvert.SerializeObject(new {
+                        message = "Failed to check for updates",
+                        exceptionInfo = ex.ToString(),
+                    }));
+
+                    return -1;
+                }
+
+                Console.WriteLine(JsonConvert.SerializeObject(new {
+                    updateInfo = updateInfo,
+                    releaseNotes = updateInfo.FetchReleaseNotes(),
+                }));
+
+                return 0;
+            }
+
+            throw new Exception("How even did we get here?");
+        }
+
+        static string determineAppName()
+        {
+            throw new NotImplementedException();
         }
 
         static bool isAUrl(string possiblyAUrl)
