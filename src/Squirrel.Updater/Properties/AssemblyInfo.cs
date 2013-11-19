5ddd70ec786cda2cce58851bc86027c4d5716bd2
Paul Betts <paul@paulbetts.org>
2013-11-19 14:32:59 -0800

    Fix the versioning info

--- src/Squirrel.Updater/Properties/AssemblyInfo.cs
+++ src/Squirrel.Updater/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Squirrel.Updater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Standalone Updater executable for Squirrel, for non-.NET apps")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Squirrel.Updater")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.4")]
+[assembly: AssemblyFileVersion("0.7.4")]
+[assembly: AssemblyInformationalVersion("0.7.4")]
