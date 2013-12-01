2f5ea6b6e2c522a8f68c26ace57e20f86e2c64c5
Bradley Grainger <bgrainger@gmail.com>
2013-12-01 14:20:01 -0800

    Fix temp paths.
    The path 'ShimmerDesktopDemo' is hard-coded in 'InstalledSquirrelDesktopDemo-1.0.0.zip'.

--- src/Squirrel.Tests/Core/DeltaPackageTests.cs
+++ src/Squirrel.Tests/Core/DeltaPackageTests.cs
@@ -79,7 +79,7 @@ namespace Squirrel.Tests.Core
                 ReleaseEntry.BuildReleasesFile(releasesDir);
 
                 var updateManager = new UpdateManager(
-                    releasesDir, "SquirrelDesktopDemo", FrameworkVersion.Net40, installDir);
+                    releasesDir, "ShimmerDesktopDemo", FrameworkVersion.Net40, installDir);
 
                 using (updateManager) {
                     var updateInfo = updateManager.CheckForUpdate().First();
@@ -93,8 +93,8 @@ namespace Squirrel.Tests.Core
                 string referenceDir;
                 using (IntegrationTestHelper.WithFakeAlreadyInstalledApp("InstalledSquirrelDesktopDemo-1.2.0.zip", out referenceDir)) {
 
-                    var referenceVersion = Path.Combine(referenceDir, "SquirrelDesktopDemo", "app-1.2.0");
-                    var installVersion = Path.Combine(installDir, "SquirrelDesktopDemo", "app-1.2.0");
+                    var referenceVersion = Path.Combine(referenceDir, "ShimmerDesktopDemo", "app-1.2.0");
+                    var installVersion = Path.Combine(installDir, "ShimmerDesktopDemo", "app-1.2.0");
 
                     var referenceFiles = Directory.GetFiles(referenceVersion);
                     var actualFiles = Directory.GetFiles(installVersion);
